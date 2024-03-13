e03e2025257a4ff1445a52e3f16b284ea6ac7042
Shashank Sahay <sahayshashank28@gmail.com>
2024-03-13 18:51:18 +0530

    avg mapping

--- mapper.py
+++ mapper.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
-
+userprofiles = {}
 for line in sys.stdin:
     userid,itemid,rating = line.strip().split()[:3] # omiting timestamp5
-    print(userid,itemid,rating)
+    rating = int(rating)
+    if userid in userprofiles.keys():
+        userprofiles[userid]["watched"].append({itemid:rating})
+    else:
+        userprofiles[userid] = dict()
+        userprofiles[userid]["watched"] = list()
+        userprofiles[userid]["watched"].append({itemid:rating})
+for userid,data in userprofiles.items():
+
+    total_sum = sum(sum(dictionary.values()) for dictionary in data["watched"])
+    total_len = len([sum(dictionary.values()) for dictionary in data["watched"]])
+    userprofiles[userid]["avg_rating"] = total_sum/total_len
+
+for userid,data in userprofiles.items():
+    print("{"+f"{userid}:{data}"+"}")
