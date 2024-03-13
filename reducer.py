b70e7e208fd6abccaa2d1c897bb6a2938c8ec72c
Shashank Sahay <sahayshashank28@gmail.com>
2024-03-13 12:03:41 +0530

    reducer recommends

--- reducer.py
+++ reducer.py
@@ -1,36 +1,48 @@
 #!/usr/bin/env python3
 
+import json
 import sys
 
-output = dict()
-output_movie = dict()
+user_rating = dict()
+movie_rating = dict()
 
+def recommend(user_rating,movie_rating):
+    recommendation = dict()
+    for userid,data in user_rating.items():
+        avg = sum(data['ratings'])/len(data['ratings'])
+        recommendation[userid] = {'avg_rating': avg,'watched':data['movies'],'recommend':[]}
+    for movieid,movierating in movie_rating.items():
+        mov_avg = sum(movierating)/len(movierating)
+        for userid,userdata in recommendation.items():
+            if(abs(userdata['avg_rating']-mov_avg)<=0.5 and movieid not in userdata['watched']):
+                recommendation[userid]['recommend'].append({movieid : userdata['avg_rating']-mov_avg})
+            
+    for userid,userdata in recommendation.items():
+        sorted_recommend = sorted(recommendation[userid]["recommend"], key=lambda x: list(x.values())[0],reverse=True)
+        recommendation[userid]["recommend"] = sorted_recommend
+    for userid,userdata in recommendation.items():
+        print(f'{userid}\tshould watch movies with id as\t{recommendation[userid]["recommend"][:5]}')
+    formatted_json = json.dumps(recommendation, indent=4)
+    # print(formatted_json)
 for line in sys.stdin:
-
     userid,itemid,rating= line.strip().split()
-    if userid in output.keys():
-        output[userid]['rating'] += int(rating)
-        output[userid]['count'] += 1
+    rating = int(rating)
+    if userid in user_rating.keys():
+        user_rating[userid]['ratings'].append(int(rating))
+        if itemid not in user_rating[userid]['movies']:
+            user_rating[userid]['movies'].append(itemid)
     else:
-        output[userid] = dict()
-        output[userid]['rating']= int(rating)
-        output[userid]['count'] = 1
 
-    if itemid in output_movie.keys():
-        output_movie[itemid]['rating'] += int(rating)
-        output_movie[itemid]['count'] += 1
+        user_rating[userid] = dict()
+        user_rating[userid]['ratings'] = list()
+        user_rating[userid]['ratings'].append(int(rating))
+        user_rating[userid]['movies'] = list()
+        user_rating[userid]['movies'].append(itemid)
+
+    if itemid in movie_rating.keys():
+        movie_rating[itemid].append(int(rating))
     else:
-        output_movie[itemid] = dict()
-        output_movie[itemid]['rating']= int(rating)
-        output_movie[itemid]['count'] = 1
-print("user\tcount\taverage rating")
-for user in output.keys():
-    average_rating_user = round(float(output[user]['rating'])/output[user]['count'])
-    if(average_rating_user>=5):
-        print(f"{user}\t{output[user]['count']}\t{average_rating_user}")
-print("\n\n")
-print("movie\tcount\taverage rating")
-for movie in output_movie.keys():
-    average_rating_movie = round(float(output_movie[movie]['rating'])/output_movie[movie]['count'])
-    if(output_movie[movie]['count']>=10 and average_rating_movie>=4):
-        print(f"{movie}\t{output_movie[movie]['count']}\t{average_rating_movie}")
+        movie_rating[itemid] = list()
+        movie_rating[itemid].append(int(rating))
+
+recommend(user_rating,movie_rating)
